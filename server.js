0f1491e6fa832d6d5f4f683244faba5bcd5e1680
Reyyahs <133382184+Reyyahs@users.noreply.github.com>
2023-08-16 02:13:33 -0400

    added middleware

--- server.js
+++ server.js
@@ -4,3 +4,7 @@ const fsPromises = require("fs").promises;
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
