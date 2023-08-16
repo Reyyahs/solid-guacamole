555d3ed2d029cfd681af5a8b2f3ec5c09a084e92
Reyyahs <133382184+Reyyahs@users.noreply.github.com>
2023-08-16 02:17:33 -0400

    added route for notes.html

--- server.js
+++ server.js
@@ -12,4 +12,8 @@ app.use(express.static("public"));
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
   });
+
+  app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "notes.html"));
+  });
   
